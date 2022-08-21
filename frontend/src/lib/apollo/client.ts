885eae2e9cca92159bf613ea953490412433866d
Diego Mais <diegomais.dev@gmail.com>
2022-08-21 16:37:32 -0300

    build(frontend): add production environment variables

--- frontend/src/lib/apollo/client.ts
+++ frontend/src/lib/apollo/client.ts
@@ -2,21 +2,23 @@ import { ApolloClient, InMemoryCache } from '@apollo/client'
 import { GraphQLWsLink } from '@apollo/client/link/subscriptions'
 import { createClient } from 'graphql-ws'
 
-const uriAuthority =
-  process.env.NEXT_PUBLIC_GRAPHQL_URI_AUTHORITY ?? 'localhost:4000'
+const httpProtocol = process.env.NEXT_PUBLIC_GRAPHQL_HTTP_PROTOCOL
+const uriAuthority = process.env.NEXT_PUBLIC_GRAPHQL_URI_AUTHORITY
+const uriPath = process.env.NEXT_PUBLIC_GRAPHQL_URI_PATH
+const wsProtocol = process.env.NEXT_PUBLIC_GRAPHQL_WS_PROTOCOL
 
 const link =
   typeof window !== 'undefined'
     ? new GraphQLWsLink(
         createClient({
-          url: `ws://${uriAuthority}/graphql`,
+          url: `${wsProtocol}://${uriAuthority}/${uriPath}`,
         })
       )
     : undefined
 
 export const apolloClient = new ApolloClient({
   cache: new InMemoryCache(),
-  uri: `http://${uriAuthority}/graphql`,
+  uri: `${httpProtocol}://${uriAuthority}/${uriPath}`,
   link,
 })
 
